c7c285606de322d58b4e2d0c8278d0d08c41d82b
littlecow27 <calebkaiho@gmail.com>
2026-06-15 09:56:43 -0400

    gamemanager code

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,33 +1,44 @@
-using Unity.VisualScripting;
-using UnityEngine;
+using Mirror;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour 
 {
     public Transform[] spawnPoints;
-    public List<GameObject> players = new List<GameObject>();
+    [SyncVar] public bool gameStarted;
+
+    public float startTimer = 1;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if(spawnPoints.Length != 2) {
             Debug.Log("Error! Spawnpoints are not equal to two.");
         }
+        gameStarted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject newPlayer = GameObject.FindGameObjectWithTag("Player");
-        if(players.Count == 0 || (newPlayer != null && !newPlayer.Equals(players[0]))) {
-            players.Add(newPlayer);
-        }
-        if(players.Count == 2) {
+        if(!gameStarted && NetworkManager.singleton.numPlayers == 2) {
             StartGame();
-            Debug.Log(players.Count);
         }
     }
 
     void StartGame() {
         Debug.Log("Game Started!");
+
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        int spawnIndex = 0;
+        foreach(GameObject player in players) {
+            player.transform.position = spawnPoints[spawnIndex].position;
+            spawnIndex++;
+        }
+        if(startTimer <= 0) {
+            gameStarted = true;
+        } else {
+            startTimer -= Time.deltaTime;
+        }
     }
 }
